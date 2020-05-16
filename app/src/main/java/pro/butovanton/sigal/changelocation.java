b75404f9285ffa96b0a75e8ec1365b2377195f34
Anton Butov <butov6101@gmail.com>
2020-05-16 15:20:10 +0300

    Карту оттестил.

--- app/src/main/java/pro/butovanton/sigal/changelocation.java
+++ app/src/main/java/pro/butovanton/sigal/changelocation.java
@@ -29,18 +29,10 @@ public class changelocation extends FragmentActivity implements OnMapReadyCallba
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
         UiSettings uiSettings = mMap.getUiSettings();
         uiSettings.setAllGesturesEnabled(true);
         uiSettings.setCompassEnabled(true);
@@ -65,6 +57,7 @@ public class changelocation extends FragmentActivity implements OnMapReadyCallba
     @Override
     protected void onResume() {
         super.onResume();
+  //      mMap.
 //        LatLng location = new LatLng(MainActivity.lantitude, MainActivity.longitude);
  //       addMarker(location);
     }
